350a0bf208533d4cffd167c4c68a265c9eecbfae
Andrew <xsaberus@gmail.com>
2025-02-08 12:26:45 +0300

    rework endpoints

--- main.py
+++ main.py
@@ -1,84 +1,10 @@
 from fastapi import FastAPI, Query, Body, HTTPException
 from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
+from hotels import router as router_hotels
 
 app = FastAPI(docs_url=None)
-
-hotels = [
-    {"id": 1, "title": "Sochi"},
-    {"id": 2, "title": "Дубай"},
-    {"id": 1, "title": "Sochi", "name": "sochi"},
-    {"id": 2, "title": "Дубай", "name": "dubai"},
-]
-
-
-@app.get("/hotels")
-def get_hotels(
-        id: int | None = Query(None, description="Айдишник"),
-        title: str | None = Query(None, description="Название отеля"),
-):
-    return [hotel for hotel in hotels if
-            (id is None or hotel["id"] == id) and (title is None or hotel["title"] == title)]
-
-
-@app.post("/hotels")
-def create_hotel(
-        title: str = Body(embed=True),
-):
-    global hotels
-    if any(hotel["title"].lower() == title.lower() for hotel in hotels):
-        raise HTTPException(
-            status_code=409,
-            detail="Отель уже существует")
-
-    hotels.append({
-        "id": hotels[-1]["id"] + 1,
-        "title": title
-    })
-    return {"status": "OK", "hotel": hotels[-1]}
-
-
-@app.put("/hotels/{hotel_id}")
-def edit_hotel(
-        hotel_id: int,
-        title: str = Body(),
-        name: str = Body(),
-):
-    global hotels
-    hotel = next((hotel for hotel in hotels if hotel["id"] == hotel_id), None)
-    if hotel is None:
-        raise HTTPException(status_code=404, detail="Отель не найден")
-
-    hotel["title"] = title
-    hotel["name"] = name
-
-    return {"status": "OK", "new_hotel_info": hotel}
-
-
-@app.patch(
-    "/hotels/{hotel_id}",
-    summary="Частичное обновление данных об отеле",
-    description="<h1>Тут мы частично обновляем данные об отеле: можно отправить name, а можно title</h1>",
-)
-def partially_edit_hotel(
-        hotel_id: int,
-        title: str | None = Body(None),
-        name: str | None = Body(None),
-):
-    global hotels
-    hotel = [hotel for hotel in hotels if hotel["id"] == hotel_id][0]
-    if title:
-        hotel["title"] = title
-    if name:
-        hotel["name"] = name
-    return {"status": "OK"}
-
-
-@app.delete("/hotels/{hotel_id}")
-def delete_hotel(hotel_id: int):
-    global hotels
-    hotels = [hotel for hotel in hotels if hotel["id"] != hotel_id]
-    return {"status": "OK"}
+app.include_router(router_hotels)
 
 
 @app.get("/docs", include_in_schema=False)
