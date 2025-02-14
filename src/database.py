599591bb5bda78a4a2a7848ab3253d68cf617898
Andrew <xsaberus@gmail.com>
2025-02-14 14:11:26 +0300

    Настройка миграции, первый запуск

--- src/database.py
+++ src/database.py
@@ -1,4 +1,11 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from src.config import settings
+from sqlalchemy.orm import DeclarativeBase
 
 engine = create_async_engine(settings.DB_URL)
+
+async_session_maker = async_sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+class Base(DeclarativeBase):
+    pass
