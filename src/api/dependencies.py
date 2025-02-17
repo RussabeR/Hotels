3b2ca8e603a173d159b07e098a4728b851043b13
Andrew <xsaberus@gmail.com>
2025-02-17 14:35:10 +0300

    Пагинация и фильтрация с помощью SQL Alchemy

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -4,8 +4,8 @@ from typing import Annotated
 
 
 class PaginationParams(BaseModel):
-    page: Annotated[int | None, Query(None, ge=1)]
-    per_page: Annotated[int | None, Query(None, ge=1, lt=20)]
+    page: Annotated[int | None, Query(1, ge=1)]
+    per_page: Annotated[int | None, Query(None, ge=1, lt=30)]
 
 
 PaginationDep = Annotated[PaginationParams, Depends()]
