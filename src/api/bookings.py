09a25ad9ece17978765eafcab7011506496fd6b1
Andrew <xsaberus@gmail.com>
2025-03-02 10:29:17 +0300

    Реализована ручка получения всех бронировний, + ручка получения бронирований для самого юзера (немного изменений)

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -5,6 +5,21 @@ from src.schemas.bookings_schema import BookingAddRequest, BookingAdd, Booking
 router = APIRouter(prefix='/bookings', tags=['Бронирование'])
 
 
+@router.get("")
+async def get_all_bookings(
+        db: DBDep
+):
+    return await db.bookings.get_all()
+
+
+@router.get("/me")
+async def get_user_bookings(
+        db: DBDep,
+        user_id: UserIdDep
+):
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
 @router.post("")
 async def add_booking(
         user_id: UserIdDep,
@@ -22,18 +37,3 @@ async def add_booking(
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
-
-
-@router.get("/bookings")
-async def get_all_bookings(
-        db: DBDep
-):
-    return await db.bookings.get_all()
-
-
-@router.get("/bookings/{user_id}")
-async def get_user_bookings(
-        db: DBDep,
-        user_id: UserIdDep
-):
-    return await db.bookings.get_filtered(user_id=user_id)
