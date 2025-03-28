0db2e9d54bbda638d73316c685474eb9971fbaee
Andrew <xsaberus@gmail.com>
2025-03-28 09:40:22 +0300

    добавление фильтра в add booking

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -5,6 +5,8 @@ from src.models.bookings import BookingsOrm
 from src.repositories.mappers.mappers import BookingDataMapper
 from sqlalchemy import select
 
+from src.repositories.utils import rooms_id_for_booking
+
 
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
@@ -17,3 +19,6 @@ class BookingsRepository(BaseRepository):
         )
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
+
+    async def add_booking(self, date_from, date_to, hotel_id):
+        rooms_ids_for_booking = rooms_id_for_booking(date_to=date_to, date_from=date_from, hotel_id=hotel_id)
